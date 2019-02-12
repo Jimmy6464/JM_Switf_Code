ba8a5d714c8a3b1c8e7cb7594c3d11bf1a59d585
YunJiSoft <yunjisoft@YunJiSoftdeiMac.local>
2019-02-12 17:01:47 +0800

    modified sth

--- JM_Swift/JM_Swift/MyPlayground.playground/Contents.swift
+++ JM_Swift/JM_Swift/MyPlayground.playground/Contents.swift
@@ -1,3 +1,33 @@
 import UIKit
 
 var str = "Hello, playground"
+var number = 1
+func balance(_ x: inout Int, _ y: inout Int) {
+    let sum = x + y
+    x = sum / 2
+    y = sum - x
+}
+var playerOneScore = 42
+var playerTwoScore = 30
+balance(&playerOneScore, &playerTwoScore)  // OK
+
+print("\(playerOneScore),\(playerTwoScore)")
+
+struct Player {
+    var name: String
+    var health: Int
+    var energy: Int
+    
+    static let maxHealth = 10
+    mutating func restoreHealth() {
+        health = Player.maxHealth
+    }
+}
+extension Player {
+    mutating func shareHealth(with teammate: inout Player) {
+        balance(&teammate.health, &health)
+    }
+}
+var oscar = Player(name: "Oscar", health: 10, energy: 10)
+var maria = Player(name: "Maria", health: 5, energy: 10)
+oscar.shareHealth(with: &maria)  // OK
